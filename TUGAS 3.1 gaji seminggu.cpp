a0083bafe2c799fa0e6c8f67ba3b8237b6d5510b
EbqEbq <38692856+EbqEbq@users.noreply.github.com>
2022-04-01 16:02:54 +0700

    Update TUGAS 3.1 gaji seminggu.cpp

--- TUGAS 3.1 gaji seminggu.cpp
+++ TUGAS 3.1 gaji seminggu.cpp
@@ -15,7 +15,7 @@ string nama;
 if (jam>=48)
   {
    lembur= jam-48   ;
-   gaji=((48*4000)+(lembur*4000));
+   gaji=((48*4000)+(lembur*5000));
    cout<<"Jam kerja anda : 48 Jam";
    cout<<"\nJam lembur anda : "<<lembur<<" jam";
    cout<<"\nGaji anda adalah : Rp. "<<gaji;
@@ -31,4 +31,18 @@ if (jam>=48)
 
 
 return 0;
-}		
+}
+
+/*Tugas 3.1 
+➢ Buatlah flowchart dan program dengan Bahasa C++ atau Bahasa C 
+untuk contoh kasus dibawah ini. 
+
+Di perusahaan PT. XXX tiap Karyawan dibayar Mingguan. Upah Karyawan 
+dihitung dari jumlah jam kerja selama seminggu. Upah Karyawan 4000/jam. 
+Jumlah jam kerja normal selama seminggu 48 jam. Kelebihan jam kerja 
+dianggap lembur, dengan upah lembur 5000/jam. 
+Buatlah Program C/C++ untuk menghitung upah yang diterima oleh seorang 
+karyawan pada akhir minggu. 
+Sebagai Input : Nama Karyawan, Jumlah jam kerja selama seminggu, 
+Perulangan jika masih ada data. 
+Sebagai Output : Total gaji selama seminggu.*/
